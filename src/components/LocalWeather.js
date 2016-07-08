f956e98dccc782e8036952d69f871e58bad5bb04
Kyle <kyleludlow@gmail.com>
2016-07-07 20:27:01 -0700

    adds basic text and icon layout for local weather view

--- src/components/LocalWeather.js
+++ src/components/LocalWeather.js
@@ -6,16 +6,34 @@ import {
   Text,
   View
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // let WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=e9d20812185bcb41aa65bb443b85ad5f'
+
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 65,
     flex: 1
   },
-  text: {
-    fontSize: 30
+  city: {
+    marginTop: 70,
+    fontSize: 40,
+    alignSelf: 'center'
+  },
+  weatherIcon: {
+    marginTop: 30,
+    alignSelf: 'center'
+  },
+  temp: {
+    marginTop: 30,
+    fontSize: 35,
+    alignSelf: 'center'
+  },
+  details: {
+    marginTop: 30,
+    fontSize: 18,
+    alignSelf: 'center'
   }
 });
 
@@ -44,9 +62,11 @@ class LocalWeather extends Component {
   render() {
     return(
       <View style={styles.container}>
-        <Text style={styles.text}>Local View</Text>
-        <Text style={styles.text}>Local View</Text>
-        <Text style={styles.text}>Local View</Text>
+        <Text style={styles.city}>San Francisco</Text>
+        <Icon style={styles.weatherIcon} name="ios-rainy-outline" size={60} color="#000" />
+        <Text style={styles.temp}>70&deg;</Text>
+        <Text style={styles.details}>Wind Speed: 2.5 mp/h</Text>
+        <Text style={styles.details}>Humidity: 30%</Text>
       </View>
     );
   }
